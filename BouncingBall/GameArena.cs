9fe9542f427e1a3b83820fa14998d44f0b94bd8b
Graham Pye <frahamp@gmail.com>
2014-03-23 22:27:06 +0000

    Added the GameAreana class

---
+++ BouncingBall/GameArena.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BouncingBall
+{
+    class GameArena
+    {
+        private int areanaWidth;
+        private int areanaHeight;
+    }
+}
