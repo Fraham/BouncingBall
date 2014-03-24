50bd70164fc3b29fd0f4232b992883fbd1f8368f
Graham Pye <frahamp@gmail.com>
2014-03-24 00:00:01 +0000

    Adding the ball class
    Adding the ball class with the constructer and variables.

---
+++ BouncingBall/Ball.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BouncingBall
+{
+    class Ball
+    {
+        private double xPosition;
+        private double yPosition;
+
+        private double size;
+
+        private string colour;
+
+        private double xSpeed;
+        private double ySpeed;
+
+        public Ball(double _xPosition, double _yPosition, double _size, String _colour, double _xSpeed, double _ySpeed)
+        {
+            xPosition = _xPosition;
+            yPosition = _yPosition;
+            size = _size;
+            colour = _colour.ToUpper();
+            xSpeed = _xSpeed;
+            ySpeed = _ySpeed;
+        }
+    }
+}
