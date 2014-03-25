c60e58caa3f61c821c37a2e641c00e1a4b4f1b4e
Graham Pye <frahamp@gmail.com>
2014-03-25 14:37:14 +0000

    Added Shape class
    Added Shape class, this will be used to model all the shapes that will
    be changed. The ball class inherits from the shape class, so methods
    have been moved over.

--- BouncingBall/Ball.cs
+++ BouncingBall/Ball.cs
@@ -6,14 +6,9 @@ namespace BouncingBall
     /// <summary>
     ///     An object used in
     /// </summary>
-    public class Ball
+    public class Ball : Shape
     {
-        private string _colour;
         private int _size;
-        private int _xPosition;
-        private int _xSpeed;
-        private int _yPosition;
-        private int _ySpeed;
 
         /// <summary>
         ///     Creating an instance of a ball.
@@ -24,50 +19,12 @@ namespace BouncingBall
         /// <param name="colour">The colour of the ball.</param>
         /// <param name="xSpeed">The speed of the ball in the x direction.</param>
         /// <param name="ySpeed">The speed of the ball in the y direction.</param>
-        public Ball(int xPosition, int yPosition, int size, String colour, int xSpeed, int ySpeed)
+        public Ball(int xPosition, int yPosition, int size, String colour, int xSpeed, int ySpeed) : base(xPosition, yPosition, colour, xSpeed, ySpeed)
         {
-            _xPosition = xPosition;
-            _yPosition = yPosition;
             _size = size;
-            _colour = colour.ToUpper();
-            _xSpeed = xSpeed;
-            _ySpeed = ySpeed;
         }
 
         #region Getters and Setters
-
-        /// <summary>
-        ///     Obtains the colour of this ball.
-        ///     Returns a colour type instead of the text version.
-        /// </summary>
-        /// <returns>The colour of this ball.</returns>
-        public Color GetColour()
-        {
-            if (_colour == "BLACK")
-            {
-                return Color.Black;
-            }
-            if (_colour == "WHITE")
-            {
-                return Color.White;
-            }
-            if (_colour == "RED")
-            {
-                return Color.Red;
-            }
-            if (_colour == "GREEN")
-            {
-                return Color.Green;
-            }
-            try
-            {
-                return Color.FromName(_colour);
-            }
-            catch
-            {
-                return Color.Black;
-            }
-        }
 
         /// <summary>
         ///     Obtains the size of this ball.
@@ -79,86 +36,9 @@ namespace BouncingBall
         }
 
         /// <summary>
-        ///     Obtains the current X position of this ball.
+        ///     Changes the current size of this ball to a given size.
         /// </summary>
-        /// <returns> The X coordinate of this Ball within the game.</returns>
-        public int GetXPosition()
-        {
-            return _xPosition;
-        }
-
-        /// <summary>
-        ///     Gets the x speed of the ball.
-        /// </summary>
-        /// <returns> The x speed of the ball.</returns>
-        public int GetXSpeed()
-        {
-            return _xSpeed;
-        }
-
-        /// <summary>
-        ///     Obtains the current Y position of this ball.
-        /// </summary>
-        /// <returns> The Y coordinate of this ball within the game.</returns>
-        public int GetYPosition()
-        {
-            return _yPosition;
-        }
-
-        /// <summary>
-        ///     Gets the x speed of the ball.
-        /// </summary>
-        /// <returns> The x speed of the ball.</returns>
-        public int GetYSpeed()
-        {
-            return _ySpeed;
-        }
-
-        /// <summary>
-        ///     Moves the current position of this Ball to the given X co-ordinate.
-        /// </summary>
-        /// <param name="x"> The new x co-ordinate of this ball.</param>
-        public void SetXPosition(int x)
-        {
-            _xPosition = x;
-        }
-
-        /// <summary>
-        ///     Changes the current speed of this Ball to the given X speed.
-        /// </summary>
-        /// <param name="x"> The new x speed of this ball.</param>
-        public void SetXSpeed(int x)
-        {
-            _xSpeed = x;
-        }
-
-        /// <summary>
-        ///     Moves the current position of this Ball to the given Y co-ordinate.
-        /// </summary>
-        /// <param name="y"> The new y co-ordinate of this ball.</param>
-        public void SetYPosition(int y)
-        {
-            _yPosition = y;
-        }
-
-        /// <summary>
-        ///     Changes the current speed of this Ball to the given y speed.
-        /// </summary>
-        /// <param name="y"> The new y speed of this ball.</param>
-        public void SetYSpeed(int y)
-        {
-            _ySpeed = y;
-        }
-
-        /// <summary>
-        ///     Changes the current colour of this ball to the given colour.
-        /// </summary>
-        /// <param name="colour">The colour being change to.</param>
-        public void SetColour(String colour)
-        {
-            _colour = colour;
-        }
-
+        /// <param name="size">The new size of the ball.</param>
         public void setSize(int size)
         {
             _size = size;
@@ -174,18 +54,18 @@ namespace BouncingBall
         /// <param name="width"> The width of the game.</param>
         public void Move(int height, int width)
         {
-            if (_xPosition + _size > width || _xPosition - _size < 0)
+            if (base.GetXPosition() + _size > width || base.GetXPosition() - _size < 0)
             {
-                _xSpeed = -_xSpeed;
+                base.SetXSpeed(-base.GetXSpeed());
             }
 
-            if (_yPosition + _size > height || _yPosition - _size < 0)
+            if (base.GetYPosition() + _size > height || base.GetYPosition() - _size < 0)
             {
-                _ySpeed = -_ySpeed;
+                base.SetYSpeed(-base.GetYSpeed());
             }
 
-            _xPosition += _xSpeed;
-            _yPosition += _ySpeed;
+            base.SetXPosition(base.GetXPosition() + base.GetXSpeed());
+            base.SetYPosition(base.GetYPosition() + base.GetYSpeed());
         }
     }
 }
---
+++ BouncingBall/Shape.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Drawing;
+
+namespace BouncingBall
+{
+    /// <summary>
+    ///     Used to model all the shapes that will be displayed.
+    /// </summary>
+    public class Shape
+    {
+        private string _colour;
+        private int _xPosition;
+        private int _xSpeed;
+        private int _yPosition;
+        private int _ySpeed;
+
+        /// <summary>
+        ///     Creating an instance of a shape.
+        /// </summary>
+        /// <param name="xPosition">The x position of the shape. Must be within border.</param>
+        /// <param name="yPosition">The y position of the shape. Must be within border.</param>
+        /// <param name="colour">The colour of the shape.</param>
+        /// <param name="xSpeed">The speed of the shape in the x direction.</param>
+        /// <param name="ySpeed">The speed of the shape in the y direction.</param>
+        public Shape(int xPosition, int yPosition, String colour, int xSpeed, int ySpeed)
+        {
+            _xPosition = xPosition;
+            _yPosition = yPosition;
+            _colour = colour.ToUpper();
+            _xSpeed = xSpeed;
+            _ySpeed = ySpeed;
+        }
+
+        /// <summary>
+        ///     Creating an instance of a shape.
+        /// </summary>
+        public Shape()
+        {
+            _xPosition = 0;
+            _yPosition = 0;
+            _colour = "WHITE";
+            _xSpeed = 0;
+            _ySpeed = 0;
+        }
+
+        #region Getters and Setters
+
+        /// <summary>
+        ///     Obtains the colour of this shape.
+        ///     Returns a colour type instead of the text version.
+        /// </summary>
+        /// <returns>The colour of this shape.</returns>
+        public Color GetColour()
+        {
+            if (_colour == "BLACK")
+            {
+                return Color.Black;
+            }
+            if (_colour == "WHITE")
+            {
+                return Color.White;
+            }
+            if (_colour == "RED")
+            {
+                return Color.Red;
+            }
+            if (_colour == "GREEN")
+            {
+                return Color.Green;
+            }
+            try
+            {
+                return Color.FromName(_colour);
+            }
+            catch
+            {
+                return Color.White;
+            }
+        }
+
+        /// <summary>
+        ///     Obtains the current X position of this shape.
+        /// </summary>
+        /// <returns> The X coordinate of this shape within the game.</returns>
+        public int GetXPosition()
+        {
+            return _xPosition;
+        }
+
+        /// <summary>
+        ///     Gets the x speed of the shape.
+        /// </summary>
+        /// <returns> The x speed of the shape.</returns>
+        public int GetXSpeed()
+        {
+            return _xSpeed;
+        }
+
+        /// <summary>
+        ///     Obtains the current Y position of this shape.
+        /// </summary>
+        /// <returns> The Y coordinate of this shape within the game.</returns>
+        public int GetYPosition()
+        {
+            return _yPosition;
+        }
+
+        /// <summary>
+        ///     Gets the x speed of the shape.
+        /// </summary>
+        /// <returns> The x speed of the shape.</returns>
+        public int GetYSpeed()
+        {
+            return _ySpeed;
+        }
+
+        /// <summary>
+        ///     Moves the current position of this shape to the given X co-ordinate.
+        /// </summary>
+        /// <param name="x"> The new x co-ordinate of this shape.</param>
+        public void SetXPosition(int x)
+        {
+            _xPosition = x;
+        }
+
+        /// <summary>
+        ///     Changes the current speed of this shape to the given X speed.
+        /// </summary>
+        /// <param name="x"> The new x speed of this shape.</param>
+        public void SetXSpeed(int x)
+        {
+            _xSpeed = x;
+        }
+
+        /// <summary>
+        ///     Moves the current position of this shape to the given Y co-ordinate.
+        /// </summary>
+        /// <param name="y"> The new y co-ordinate of this shape.</param>
+        public void SetYPosition(int y)
+        {
+            _yPosition = y;
+        }
+
+        /// <summary>
+        ///     Changes the current speed of this shape to the given y speed.
+        /// </summary>
+        /// <param name="y"> The new y speed of this shape.</param>
+        public void SetYSpeed(int y)
+        {
+            _ySpeed = y;
+        }
+
+        /// <summary>
+        ///     Changes the current colour of this shape to the given colour.
+        /// </summary>
+        /// <param name="colour">The colour being change to.</param>
+        public void SetColour(String colour)
+        {
+            _colour = colour;
+        }
+        #endregion Getters and Setters
+    }
+}
