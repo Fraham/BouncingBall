1b763749b14b558610db4eba6022b93da58147a5
Graham Pye <frahamp@gmail.com>
2014-03-31 02:00:09 +0100

    Added constructers to the Friendly class

--- BouncingBall/Friendly.cs
+++ BouncingBall/Friendly.cs
@@ -6,8 +6,26 @@ using System.Threading.Tasks;
 
 namespace BouncingBall
 {
-    class Friendly// : Rectangle
+    class Friendly : Rectangle
     {
+        /// <summary>
+        /// Creating an instance of a rectangle.
+        /// </summary>
+        /// <param name="xPosition">The x position of the rectangle. Must be within border.</param>
+        /// <param name="yPosition">The y position of the rectangle. Must be within border.</param>
+        /// <param name="height">   The height of the rectangle.</param>
+        /// <param name="width">    The width of the rectangle.</param>
+        /// <param name="colour">   The colour of the rectangle.</param>
+        /// <param name="xSpeed">   The speed of the rectangle in the x direction.</param>
+        /// <param name="ySpeed">   The speed of the rectangle in the y direction.</param>
+        public Friendly(int xPosition, int yPosition, String colour, int xSpeed, int ySpeed, int height, int width)
+            : base(xPosition, yPosition, colour, xSpeed, ySpeed, height, width)
+        { }
 
+        /// <summary>
+        /// Creating an instance of a rectangle.
+        /// </summary>
+        public Friendly()
+        { }
     }
 }
