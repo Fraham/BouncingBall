80603c645607fd5090e0d473a4fca23869f7dcf3
Graham Pye <frahamp@gmail.com>
2014-04-02 00:20:04 +0100

    Adding constructors and methods
    Adding constructors and methods to the Polygon class.

--- BouncingBall/Shapes/Polygon.cs
+++ BouncingBall/Shapes/Polygon.cs
@@ -4,9 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BouncingBall.Shapes
+namespace BouncingBall
 {
-    class Polygon
+    public class Polygon : Shape
     {
+        public Polygon(int xPosition, int yPosition, String colour, int xSpeed, int ySpeed, int height, int width, System.Drawing.Point[] points)
+            : base(xPosition, yPosition, colour, xSpeed, ySpeed, height, width)
+        { }
+
+        public static float FindMinX(System.Drawing.Point[] points)
+        {
+            return 0;
+        }
+
+        public static float FindMinY(System.Drawing.Point[] points)
+        {
+            return 0;
+        }
+
+        public static float FindWidth(System.Drawing.Point[] points)
+        {
+            return 0;
+        }
+
+        public static float FindHeight(System.Drawing.Point[] points)
+        {
+            return 0;
+        }
     }
 }
