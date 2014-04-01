bfebadb60c7011cedcbe1d413fa0eb6c67e70c98
Graham Pye <frahamp@gmail.com>
2014-04-02 00:24:53 +0100

    Adding comments

--- BouncingBall/Shapes/Polygon.cs
+++ BouncingBall/Shapes/Polygon.cs
@@ -12,21 +12,41 @@ namespace BouncingBall
             : base(xPosition, yPosition, colour, xSpeed, ySpeed, height, width)
         { }
 
+        /// <summary>
+        /// Finds the minimum x value to be needed in creating a polygon.
+        /// </summary>
+        /// <param name="points">The points of the polygon.</param>
+        /// <returns>The minimum x value.</returns>
         public static float FindMinX(System.Drawing.Point[] points)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Finds the minimum y value to be needed in creating a polygon.
+        /// </summary>
+        /// <param name="points">The points of the polygon.</param>
+        /// <returns>The minimum y value.</returns>
         public static float FindMinY(System.Drawing.Point[] points)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Finds the width value to be needed in creating a polygon.
+        /// </summary>
+        /// <param name="points">The points of the polygon.</param>
+        /// <returns>The width of the polygon.</returns>
         public static float FindWidth(System.Drawing.Point[] points)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Finds the height value to be needed in creating a polygon.
+        /// </summary>
+        /// <param name="points">The points of the polygon.</param>
+        /// <returns>The height of the polygon.</returns>
         public static float FindHeight(System.Drawing.Point[] points)
         {
             return 0;
