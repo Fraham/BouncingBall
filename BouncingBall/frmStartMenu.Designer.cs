cdc4701957aa55328b1b13f98cce954caf5a362b
Graham Pye <frahamp@gmail.com>
2014-04-19 19:55:11 +0100

    Setting up the table layout panel
    Going to be used to hold the buttons to start of the game and the
    options menu.

--- BouncingBall/frmStartMenu.Designer.cs
+++ BouncingBall/frmStartMenu.Designer.cs
@@ -28,13 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tlpStart = new System.Windows.Forms.TableLayoutPanel();
             this.SuspendLayout();
+            // 
+            // tlpStart
+            // 
+            this.tlpStart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tlpStart.ColumnCount = 2;
+            this.tlpStart.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpStart.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 140F));
+            this.tlpStart.Location = new System.Drawing.Point(0, 0);
+            this.tlpStart.Name = "tlpStart";
+            this.tlpStart.RowCount = 1;
+            this.tlpStart.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpStart.Size = new System.Drawing.Size(283, 265);
+            this.tlpStart.TabIndex = 0;
             // 
             // frmStartMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.tlpStart);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "frmStartMenu";
             this.Text = "Start Menu";
@@ -43,5 +60,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tlpStart;
+
     }
 }
