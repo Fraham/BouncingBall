9f3a9908b86aa07b9247c239fed0df6f902f4670
Graham Pye <frahamp@gmail.com>
2014-04-19 20:04:52 +0100

    Completed messing with the controls

--- BouncingBall/frmStartMenu.Designer.cs
+++ BouncingBall/frmStartMenu.Designer.cs
@@ -40,31 +40,39 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tlpStart.ColumnCount = 2;
-            this.tlpStart.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tlpStart.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpStart.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tlpStart.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
             this.tlpStart.Controls.Add(this.btnStart, 0, 0);
             this.tlpStart.Controls.Add(this.btnOptions, 1, 0);
             this.tlpStart.Location = new System.Drawing.Point(0, 0);
             this.tlpStart.Name = "tlpStart";
             this.tlpStart.RowCount = 1;
             this.tlpStart.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tlpStart.Size = new System.Drawing.Size(283, 265);
+            this.tlpStart.Size = new System.Drawing.Size(292, 48);
             this.tlpStart.TabIndex = 0;
             // 
             // btnStart
             // 
+            this.btnStart.AutoEllipsis = true;
+            this.btnStart.AutoSize = true;
+            this.btnStart.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnStart.Location = new System.Drawing.Point(3, 3);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.Size = new System.Drawing.Size(162, 41);
             this.btnStart.TabIndex = 0;
             this.btnStart.Text = "Start Game";
             this.btnStart.UseVisualStyleBackColor = true;
             // 
             // btnOptions
             // 
-            this.btnOptions.Location = new System.Drawing.Point(144, 3);
+            this.btnOptions.AutoEllipsis = true;
+            this.btnOptions.AutoSize = true;
+            this.btnOptions.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnOptions.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOptions.Location = new System.Drawing.Point(171, 3);
             this.btnOptions.Name = "btnOptions";
-            this.btnOptions.Size = new System.Drawing.Size(75, 23);
+            this.btnOptions.Size = new System.Drawing.Size(118, 41);
             this.btnOptions.TabIndex = 0;
             this.btnOptions.Text = "Options";
             this.btnOptions.UseVisualStyleBackColor = true;
@@ -73,12 +81,13 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(293, 45);
             this.Controls.Add(this.tlpStart);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "frmStartMenu";
             this.Text = "Start Menu";
             this.tlpStart.ResumeLayout(false);
+            this.tlpStart.PerformLayout();
             this.ResumeLayout(false);
 
         }
